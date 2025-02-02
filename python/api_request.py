157593141bde7de372a3e0d2373adbcf864e89fb
Le Ba Tien Trien <lbtientrien@gmail.com>
2025-02-02 13:57:51 +0700

    Update

---
+++ python/api_request.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+import time
+import pytz
+# Library for Data Analysis
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+# Library for POST GET & Socket requests
+import requests
+# import urllib.parse
+import json
+import socket
+# Connect to mariaDB to perform CRUD
+import mariadb
